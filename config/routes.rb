6ce4853620de2c54b020f2fb48a291b3ff14e7e5
Amy Weiner <arweiner@alumni.stanford.edu>
2014-04-11 15:47:49 -0700

    Remove foundation gem and uninstall foundation from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/about', to: 'home#about', as: 'about'
+  get '/contact', to: 'home#contact', as: 'contact'
   devise_for :admins
   devise_for :users
 end
