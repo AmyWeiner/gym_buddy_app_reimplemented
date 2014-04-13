4b658c0c3d5667858276300e35eec8c8042bff2b
Amy Weiner <arweiner@alumni.stanford.edu>
2014-04-12 22:25:42 -0700

    Add time to availability

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -5,7 +5,7 @@ class AvailabilitiesController < ApplicationController
   end
 
   def create
-    new_availability = params.require(:availability).permit(:day)
+    new_availability = params.require(:availability).permit(:day, :time)
     @user = current_user
     @availability = @user.availabilities.create(new_availability)
     redirect_to current_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412021554) do
+ActiveRecord::Schema.define(version: 20140413033246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20140412021554) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "hour"
+    t.time     "time"
   end
 
   create_table "gyms", force: true do |t|
